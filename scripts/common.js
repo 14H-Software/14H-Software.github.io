321d4badabbc1f61027f0a60fe1ded3f4bec2331
14H <128462741+d3gk@users.noreply.github.com>
2024-09-04 17:55:10 +0300

    040924_175505

--- scripts/common.js
+++ scripts/common.js
@@ -4,13 +4,34 @@ function ByID(id) {
     return document.getElementById(id);
 }
 
+function AddClass(elementID, className) {
+    if (!elementName || !classname) {
+        return false;
+    }
+    var el2mod = ByID(elementID);
+    el2mod.element.classList.add(className);
+    return true;
+}
+
+function RemoveClass(elementID, className) {
+    if (!elementName || !classname) {
+        return false;
+    }
+    var el2mod = ByID(elementID);
+    el2mod.element.classList.remove(className);
+    return true;
+}
+
+
 function JsNavigate(tab) {
     var pagecont = ByID('PageTabContainer');
     if (!tab) {
-        pagecont.style.visibility = 'collapse';
+        ByID('PageTabContainer').style.width = '0%';
+        ByID('PageTabContainer').style.opacity = '0';
         return;
     }
-    pagecont.style.visibility = 'visible';
+    ByID('PageTabContainer').style.width = '100%'
+    ByID('PageTabContainer').style.opacity = '1';
     pagecont.src = './tabs/' + tab + '.html';
 }
 
@@ -18,20 +39,60 @@ function JsPopups(tab) {
     var pucont = ByID('PopupContainer');
     if (!tab) {
         pucont.style.visibility = 'collapse';
+        ByID('PopupContainer').opacity = 0;
+        ByID('JsPopupFrame').style.visibility = 'collapse';
+        ByID('JsStdPopup').style.visibility = 'collapse';
+        ByID('JsStdPopupBtn').style.visibility = 'collapse';
         return;
     }
     pucont.style.visibility = 'visible';
-    pucont.src = './popups/' + tab + '.html';
+    ByID('PopupContainer').opacity = 1;
+    ByID('JsPopupFrame').style.visibility = 'visible';
+    ByID('JsStdPopup').style.visibility = 'collapse';
+    ByID('JsPopupFrame').src = './popups/' + tab + '.html';
 }
+
+function JsStandardPopups(title, message, button, isbtnenabled) {
+    ByID('PopupContainer').style.visibility = 'visible';
+    ByID('JsStdPopupBtn').style.visibility = 'visible';
+    ByID('PopupContainer').opacity = 1;
+    ByID('JsStdPopupTitle').textContent = title;
+    ByID('JsStdPopupDesc').textContent = message;
+    ByID('JsStdPopupBtn').textContent = button;
+    ByID('JsStdPopup').style.visibility = 'visible';
+    ByID('PopupStdBtn').style.visibility = isbtnenabled ? 'visible' : 'collapse';
+    ByID('JsStdPopupBtn').style.visibility = 'visible';
+    ByID('JsStdPopupBtn').style.opacity = '1';
+} 
+
+function JsStandardPopupsNoBtn(title, message, button, isbtnenabled) {
+    ByID('PopupContainer').style.visibility = 'visible';
+    ByID('JsStdPopupBtn').style.visibility = 'visible';
+    ByID('PopupContainer').opacity = 1;
+    ByID('JsStdPopupTitle').textContent = title;
+    ByID('JsStdPopupDesc').textContent = message;
+    ByID('JsStdPopupBtn').textContent = button;
+    ByID('JsStdPopup').style.visibility = 'visible';
+    ByID('JsStdPopupBtn').style.visibility = 'collapse';
+    ByID('JsStdPopupBtn').style.opacity = '0';
+} 
+
 
 function IsMobile() {
     return /Mobi|Android/i.test(navigator.userAgent);
 }
 
 function IsMainentance() {
-    return false;
+    return true;
 }
 
 function Init() {
     JsNavigate(false);
+    JsPopups(false);
+    if (IsMainentance()) {
+        JsStandardPopupsNoBtn('MAINTENANCE', '14H Website is temporarily unavailable. Please try again later.', 'Continiue', false);
+    }
+    if (IsMobile) {
+        JsStandardPopupsNoBtn('14H', '14H Website is unavailable on mobile devices', '', false);
+    }
 }
